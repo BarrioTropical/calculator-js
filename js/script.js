535f729b287a400d449ec92080e35c156d8920e3
“Barrio <barriotropical@gmail.com>
2024-02-21 22:33:35 +0100

    js | keys display
    Signed-off-by: “Barrio <barriotropical@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // values
 const calculator = {
-    displayValue: 'O',
+    displayValue: '0',
     firstOperand: null,
     waitingForSecondOperand: false,
     operator: null,
@@ -9,6 +9,82 @@ const calculator = {
 //display
 const updateDisplay = () =>{
     const display = document.querySelector('.screen');
-    display.value = calculator.displayValue
+    display.value = calculator.displayValue;
 };
 updateDisplay();
+
+// when we press keys display the value
+const keys = document.querySelector('.keys');
+keys.addEventListener('click', (event) =>{
+    const { target } = event;
+    if(!target.matches('button')){
+        return;
+    }
+
+    if(target.classList.contains('operator')) {
+        handleOperator(target.value)
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        resetCalculator()
+        updateDisplay()
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
+
+//input digit operations
+const inputDigit = (digit) => {
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if(waitingForSecondOperand === true){
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false
+    } else{
+        calculator.displayValue = 
+        displayValue === '0' ? digit : displayValue + digit;
+    }
+};
+
+//input decimal operations
+const inputDecimal = (dot) => {
+    if(calculator.waitingForSecondOperand === true){
+        calculator.displayValue = '0.';
+        calculator.waitingForSecondOperand = false;
+        return;
+    }
+    if(!calculator.displayValue.includes(dot)){
+        calculator.displayValue += dot;
+    }
+};
+
+// Handle Operators
+const handleOperator = (nextOperator) => {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue)
+
+    if(operator && calculator.waitingForSecondOperand){
+        calculator.operator = nextOperator;
+        return
+    }
+    if(firstOperand = null && !isNaN(inputValue)){
+        calculator.firstOperand = inputValue
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator)
+        calculator.displayValue = `${parseFloat(result.toFixed(7))}`
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+};
